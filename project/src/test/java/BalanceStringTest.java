2a7ddd3a1e72400aebb1a0a6422f33994bd2ca9b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-04-15 05:47:21 +0800

    Optimize test cases

--- project/src/test/java/BalanceStringTest.java
+++ project/src/test/java/BalanceStringTest.java
@@ -1,72 +1,21 @@
 import org.example.BalanceString;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BalanceStringTest {
 
-    @Test
-    public void testCase1() {
+    @ParameterizedTest
+    @CsvSource({
+            "'W?WR?', 'R??W?', 3",
+            "'R?R??', '??W??', 5",
+            "'RR?', '?R?', -1",
+            "'WWW??', 'RRR??', -1",
+            "'?RW?WR', '?WR?RW', 0'"
+    })
 
-        String row1 = "W?WR?";
-        String row2 = "R??W?";
-        int expected = 3;
-
-        BalanceString solution = new BalanceString();
-        int result = solution.solution(row1, row2);
-
-        assertEquals(expected, result);
-
-    }
-
-    @Test
-    public void testCase2() {
-
-        String row1 = "R?R??";
-        String row2 = "??W??";
-        int expected = 5;
-
-        BalanceString solution = new BalanceString();
-        int result = solution.solution(row1, row2);
-
-        assertEquals(expected, result);
-
-    }
-
-    @Test
-    public void testCase3() {
-
-        String row1 = "RR?";
-        String row2 = "?R?";
-        int expected = -1;
-
-        BalanceString solution = new BalanceString();
-        int result = solution.solution(row1, row2);
-
-        assertEquals(expected, result);
-
-    }
-
-    @Test
-    public void testCase4() {
-
-        String row1 = "WWW??";
-        String row2 = "RRR??";
-        int expected = -1;
-
-        BalanceString solution = new BalanceString();
-        int result = solution.solution(row1, row2);
-
-        assertEquals(expected, result);
-
-    }
-
-    @Test
-    public void testCase5() {
-
-        String row1 = "?RW?WR";
-        String row2 = "?WR?RW";
-        int expected = 0;
+    public void testCases(String row1, String row2, int expected) {
 
         BalanceString solution = new BalanceString();
         int result = solution.solution(row1, row2);
